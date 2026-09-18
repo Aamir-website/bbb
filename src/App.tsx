3f9048f95ef3f919cf44dc55f6c4a53bf4056699
Aamir-website <doozboi99@gmail.com>
2026-09-18 18:41:14 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -298,7 +298,7 @@ function App() {
             fit="contain"
             className="desktop-image hero-image-layer fixed no-parallax-y hero-design-text"
             style={{ inset: 0, width: '100%', height: '100%', zIndex: 20 }}
-            narrownessScale={1}
+            narrownessScale={0}
             narrownessTranslateY={1}
           />
         </div>
